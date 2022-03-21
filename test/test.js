37e18dc09ac9462ea95a93a815345ce92bb8dba5
Samet Kamgul <sametkamgul@gmail.com>
2022-03-21 20:00:40 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,3 +3,7 @@ var test = require('../server');
 test.info("info-1", "info-2");
 test.error("error-1", "error-2");
 test.warn("warn-1", "wan-2");
+
+var testRemote = require('kerevizlog');
+
+testRemote.info('info-remote-1', 'info-remote-2');
